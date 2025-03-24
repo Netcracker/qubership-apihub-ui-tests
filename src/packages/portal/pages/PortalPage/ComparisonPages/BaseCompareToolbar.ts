3c0dcefdcdfdf6726a60628a6e2b0e38bf392a2a
Vladyslav Novikov <48720535+vOrigins@users.noreply.github.com>
2025-03-24 11:20:01 +0200

    fix: rename 'risky' changes to 'requires attention'

--- src/packages/portal/pages/PortalPage/ComparisonPages/BaseCompareToolbar.ts
+++ src/packages/portal/pages/PortalPage/ComparisonPages/BaseCompareToolbar.ts
@@ -8,12 +8,12 @@ export abstract class BaseCompareToolbar {
   readonly breadcrumbs = new Breadcrumbs(this.locator.getByTestId('ComparedPackagesBreadcrumbs'), 'Comparison')
   readonly backBtn = new Button(this.locator.getByTestId('BackButton'), 'Back')
   readonly packageSlt = new ComparisonPagePackageSelect(this.page)
-  readonly breakingChangesFilterBtn = new Button(this.locator.getByTestId('ChangesFilterButton-breaking'), 'Breaking changes filter')
-  readonly semiBreakingChangesFilterBtn = new Button(this.page.getByTestId('ChangesFilterButton-semi-breaking'), 'Risky changes filter')
-  readonly deprecatedChangesFilterBtn = new Button(this.locator.getByTestId('ChangesFilterButton-deprecated'), 'Deprecated changes filter')
-  readonly nonBreakingChangesFilterBtn = new Button(this.locator.getByTestId('ChangesFilterButton-non-breaking'), 'Non-breaking changes filter')
-  readonly annotationChangesFilterBtn = new Button(this.locator.getByTestId('ChangesFilterButton-annotation'), 'Annotation changes filter')
-  readonly unclassifiedChangesFilterBtn = new Button(this.locator.getByTestId('ChangesFilterButton-unclassified'), 'Unclassified changes filter')
+  readonly breakingChangesFilterBtn = new Button(this.locator.getByTestId('ChangesFilterButton-breaking'), 'Breaking Сhanges filter')
+  readonly semiBreakingChangesFilterBtn = new Button(this.page.getByTestId('ChangesFilterButton-semi-breaking'), 'Changes Requiring Attention filter')
+  readonly deprecatedChangesFilterBtn = new Button(this.locator.getByTestId('ChangesFilterButton-deprecated'), 'Deprecated Сhanges filter')
+  readonly nonBreakingChangesFilterBtn = new Button(this.locator.getByTestId('ChangesFilterButton-non-breaking'), 'Non-breaking Сhanges filter')
+  readonly annotationChangesFilterBtn = new Button(this.locator.getByTestId('ChangesFilterButton-annotation'), 'Annotation Сhanges filter')
+  readonly unclassifiedChangesFilterBtn = new Button(this.locator.getByTestId('ChangesFilterButton-unclassified'), 'Unclassified Сhanges filter')
 
   protected constructor(protected readonly page: Page) { }
 }
--- src/packages/portal/pages/PortalPage/VersionPage/VersionOverviewTab/OverviewSummaryTab/OverviewSummaryTabBody/OverviewValidationsContent.ts
+++ src/packages/portal/pages/PortalPage/VersionPage/VersionOverviewTab/OverviewSummaryTab/OverviewSummaryTabBody/OverviewValidationsContent.ts
@@ -7,12 +7,12 @@ export class OverviewValidationsContent {
   readonly deprecatedOperations = new Content(this.locator.getByTestId('NumberOfDeprecatedOperationsTypography'), 'Number of deprecated operations')
   readonly noBwcOperations = new Content(this.locator.getByTestId('NumberOfNoBwcOperationsTypography'), 'Number of no-BWC operations')
   readonly bwcErrors = new Content(this.locator.getByTestId('NumberOfBwcErrorsTypography'), 'Number of BWC errors')
-  readonly breakingChanges = new Content(this.locator.getByTestId('breaking'), 'Breaking changes')
-  readonly semiBreakingChanges = new Content(this.locator.getByTestId('semi-breaking'), 'Risky changes')
-  readonly deprecatedChanges = new Content(this.locator.getByTestId('deprecated'), 'Deprecated changes')
-  readonly nonBreakingChanges = new Content(this.locator.getByTestId('non-breaking'), 'Non-breaking changes')
-  readonly annotationChanges = new Content(this.locator.getByTestId('annotation'), 'Annotation changes')
-  readonly unclassifiedChanges = new Content(this.locator.getByTestId('unclassified'), 'Unclassified changes')
+  readonly breakingChanges = new Content(this.locator.getByTestId('breaking'), 'Breaking Сhanges')
+  readonly semiBreakingChanges = new Content(this.locator.getByTestId('semi-breaking'), 'Changes Requiring Attention')
+  readonly deprecatedChanges = new Content(this.locator.getByTestId('deprecated'), 'Deprecated Сhanges')
+  readonly nonBreakingChanges = new Content(this.locator.getByTestId('non-breaking'), 'Non-breaking Сhanges')
+  readonly annotationChanges = new Content(this.locator.getByTestId('annotation'), 'Annotation Сhanges')
+  readonly unclassifiedChanges = new Content(this.locator.getByTestId('unclassified'), 'Unclassified Сhanges')
 
   constructor(private readonly locator: Locator) { }
 }
