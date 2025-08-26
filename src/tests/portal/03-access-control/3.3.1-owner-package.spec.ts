8a1c7a866dd2910cbc1f7119ff1cd927ad7600d5
Iurii Golovinskii <iurii.golovinskii.qubership@gmail.com>
2025-08-26 19:06:59 +0300

    fix: adaptation to changes in backend authorization (#50)

--- src/tests/portal/03-access-control/3.3.1-owner-package.spec.ts
+++ src/tests/portal/03-access-control/3.3.1-owner-package.spec.ts
@@ -349,12 +349,6 @@ test.describe('03.3.1 Access Control. Owner role. (Package)', () => {
         await expect(portalPage.tooltip).toHaveCount(1)
         await expect(portalPage.tooltip).toHaveText(NO_PERM_GEN_TOKEN)
 
-        await accessTokensTab.getTokenRow(1).hover()
-
-        await expect(accessTokensTab.getTokenRow(1).deleteBtn).toBeDisabled()
-
-        await accessTokensTab.getTokenRow(1).deleteBtn.hover({ force: true })
-
         await expect(portalPage.tooltip).toHaveCount(1)
         //! await expect(portalPage.tooltip).toHaveText(NO_PERM_REVOKE_TOKEN) //Issue: TestCase-B-1019
       })
@@ -368,21 +362,6 @@ test.describe('03.3.1 Access Control. Owner role. (Package)', () => {
 
         await expect(portalPage.tooltip).toHaveCount(1)
         await expect(portalPage.tooltip).toHaveText(NO_PERM_ADD_MEMBER)
-
-        await accessControlTab.getUserRow(1).hover()
-
-        await expect(accessControlTab.getUserRow(1).adminChx).toBeDisabled()
-        await expect(accessControlTab.getUserRow(1).deleteBtn).toBeDisabled()
-
-        await accessControlTab.getUserRow(1).adminChx.hover({ force: true })
-
-        await expect(portalPage.tooltip).toHaveCount(1)
-        await expect(portalPage.tooltip).toHaveText(NO_PERM_MANAGE_ROLES)
-
-        await accessControlTab.getUserRow(1).deleteBtn.hover({ force: true })
-
-        await expect(portalPage.tooltip).toHaveCount(1)
-        await expect(portalPage.tooltip).toHaveText(NO_PERM_MANAGE_ROLES)
       })
     })
 
