ea23f5bbf86cfd7bfab87afa52864f68bcefa77a
Iurii Golovinskii <iurii.golovinskii.qubership@gmail.com>
2026-03-30 16:50:57 +0300

    fix: tests adapted for new global search form (#82)

--- src/packages/portal/pages/PortalPage/GlobalSearchPanel/GsFilters.ts
+++ src/packages/portal/pages/PortalPage/GlobalSearchPanel/GsFilters.ts
@@ -1,11 +1,7 @@
 import type { Locator } from '@playwright/test'
-import { Autocomplete, Button, Checkbox, DatePicker } from '@shared/components/base'
+import { Autocomplete, Button, DatePicker } from '@shared/components/base'
 import { GsVersionStatusAutocomplete } from './GsFilters/GsVersionStatusAutocomplete'
 import { ApiTypeAutocomplete } from '@portal/components/autocompletes/ApiTypeAutocomplete'
-import { GsScopeAutocomplete } from './GsFilters/GsScopeAutocomplete'
-import { GsDetailedScopeAutocomplete } from './GsFilters/GsDetailedScopeAutocomplete'
-import { GsMethodsAutocomplete } from './GsFilters/GsMethodsAutocomplete'
-import { GsOperationTypesAutocomplete } from './GsFilters/GsOperationTypesAutocomplete'
 
 export class GsFilters {
 
@@ -15,12 +11,7 @@ export class GsFilters {
   readonly acVersion = new Autocomplete(this.locator.getByTestId('PackageVersionAutocomplete'), 'Package version')
   readonly acVersionStatus = new GsVersionStatusAutocomplete(this.locator)
   readonly datePicker = new DatePicker(this.locator.getByTestId('DatePicker'), 'Global Search')
-  readonly chxSearchOnly = new Checkbox(this.locator.getByTestId('SearchOnlyCheckbox'), 'Search only')
   readonly acApiType = new ApiTypeAutocomplete(this.locator)
-  readonly acScope = new GsScopeAutocomplete(this.locator)
-  readonly acDetailedScope = new GsDetailedScopeAutocomplete(this.locator)
-  readonly acMethods = new GsMethodsAutocomplete(this.locator)
-  readonly acOperationTypes = new GsOperationTypesAutocomplete(this.locator)
   readonly btnReset = new Button(this.locator.getByTestId('ResetButton'), 'Reset')
 
   constructor(private readonly locator: Locator) { }
